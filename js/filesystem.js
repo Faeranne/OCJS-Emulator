3451a21ddfa456728c6e84bf5308b348501d7460
Blixa Morgan <blixa@projectmakeit.com>
2017-11-09 18:15:16 -0600

    WIP

--- js/filesystem.js
+++ js/filesystem.js
@@ -4,6 +4,41 @@ let FileSystem = function(newLoader){
   this.UI = document.createElement('div');
 
   let files = {};
+  let label = "Disk";
+
+  this.methods.list = function(path){
+
+  }
+
+  this.methods.open = function(path,mode){
+
+  }
+
+  this.methods.read = function(handle,count){
+
+  }
+  
+  this.methods.write = function(handle,value){
+
+  }
+
+  this.methods.exists = function(path){
+
+  }
+
+  this.methods.rename = function(from,to){
+
+  }
+
+  this.methods.close = function(handle){
+
+  }
+  
+  this.methods.getLabel = function(){
+
+  }
+
+  this.methods.setLabel = function(newLabel){
   
   return this;
 }
